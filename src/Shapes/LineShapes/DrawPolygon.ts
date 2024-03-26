be7ae144dd9062049c644432f7ea52d90e4496de
mikegani <michael.g.perfetto@gmail.com>
2024-03-26 12:52:26 +0200

    drag polygon functionality

--- src/Shapes/LineShapes/DrawPolygon.ts
+++ src/Shapes/LineShapes/DrawPolygon.ts
@@ -1,4 +1,6 @@
+import { DrawManagerMode } from "../../enums/DrawManagerMode";
 import { Shapes } from "../../enums/Shapes";
+import { DrawShape } from "../DrawShape";
 import { DrawLineShape } from "./DrawLineShape";
 import L from "leaflet";
 
@@ -7,6 +9,8 @@ import L from "leaflet";
  */
 class DrawPolygon extends DrawLineShape<L.Polygon> {
   private static instance: DrawPolygon | null = null;
+  private dragMarker: L.Marker | null;
+  private isDraggingCenterMarker: boolean;
 
   /**
    * Creates a new instance of DrawPolygon.
@@ -20,6 +24,8 @@ class DrawPolygon extends DrawLineShape<L.Polygon> {
     shapeOptions: L.PolylineOptions
   ) {
     super(map, featureGroup, shapeOptions, Shapes.POLYGON);
+    this.dragMarker = null;
+    this.isDraggingCenterMarker = false;
   }
 
   /**
@@ -33,6 +39,7 @@ class DrawPolygon extends DrawLineShape<L.Polygon> {
     featureGroup: L.FeatureGroup,
     shapeOptions: L.PolylineOptions
   ): DrawPolygon {
+    DrawShape.validateInstanceCall();
     if (!DrawPolygon.instance) {
       DrawPolygon.instance = new DrawPolygon(map, featureGroup, shapeOptions);
     }
@@ -54,6 +61,7 @@ class DrawPolygon extends DrawLineShape<L.Polygon> {
    */
   override stopDrawing(): void {
     super.stopDrawing();
+    this.removeDragMarker();
     DrawPolygon.instance = null;
   }
 
@@ -67,6 +75,131 @@ class DrawPolygon extends DrawLineShape<L.Polygon> {
     this.featureGroup.addLayer(polygon);
 
     return polygon;
+  }
+
+  private addDragMarker() {
+    if (!this.currentShape || this.latLngs.length < 3 || this.dragMarker) return;
+    // Get the bounds of the polygon
+    var polygonBounds = this.currentShape.getBounds();
+
+    // Get the center of the polygon
+    var polygonCenter = polygonBounds.getCenter();
+    if (!polygonCenter) return;
+
+    const MarkerOptions = {
+      draggable: true,
+      icon: L.divIcon({
+        className: "vertex-marker",
+        html: ` `,
+        iconSize: L.point(30, 30),
+      }),
+    };
+
+    var marker = L.marker(polygonCenter, MarkerOptions).addTo(this.featureGroup);
+    marker.on(
+      "dragstart",
+      function (event) {
+        this.isDraggingCenterMarker = true;
+        this.vertices.clearAllVertices();
+        this.disableDrawEvents();
+        this.removeDashedPolyline();
+      }.bind(this)
+    );
+
+    // When the marker is dragged, update the polygon's position
+    marker.on("drag", (event) => {
+      var markerLatLng = event.target.getLatLng();
+      var latlngs = [...this.latLngs];
+
+      // Calculate the offset between the marker's new position and the original polygon center
+      var offsetLat = markerLatLng.lat - polygonCenter.lat;
+      var offsetLng = markerLatLng.lng - polygonCenter.lng;
+
+      // Update each vertex of the polygon
+      for (var i = 0; i < latlngs.length; i++) {
+        latlngs[i].lat += offsetLat;
+        latlngs[i].lng += offsetLng;
+      }
+
+      // Set the new polygon position
+      this.latLngs = latlngs;
+
+      this.redrawShape();
+
+      // Update the polygon center
+      polygonCenter = markerLatLng;
+    });
+
+    marker.on(
+      "dragend",
+      function (event) {
+        this.isDraggingCenterMarker = false;
+        this.vertices.setLatLngs = [...this.latLngs];
+        this.vertices.drawVertices();
+        this.vertices.drawMidpointVertices();
+        if (this.drawMode !== DrawManagerMode.DRAW) return;
+        this.cursorPosition = event.target._latlng;
+        this.drawDashedPolyline();
+        setTimeout(() => {
+          this.initDrawEvents();
+          this.vertices.initDrawEvents();
+        }, 50);
+      }.bind(this)
+    );
+
+    this.dragMarker = marker;
+  }
+
+  /**
+   * Edits the given shape, adding the necessary event listeners and updating the internal state.
+   * @param shape The shape to edit.
+   */
+  override editShape(shape: L.Polygon<any>) {
+    super.editShape(shape);
+    this.addDragMarker();
+
+    return this.currentShape;
+  }
+
+  override redrawShape(): L.Polygon<any> {
+    super.redrawShape();
+    if (!this.isDraggingCenterMarker) {
+      this.removeDragMarker();
+      this.addDragMarker();
+    }
+
+    return this.currentShape;
+  }
+
+  /**
+   * Removes the drag marker from the map if it exists.
+   */
+  removeDragMarker() {
+    if (this.dragMarker) {
+      this.featureGroup.removeLayer(this.dragMarker);
+      this.dragMarker = null;
+    }
+  }
+
+  override initDrawEvents(): void {
+    super.initDrawEvents();
+    this.map.on(
+      "click",
+      function (e) {
+        if (!this.dragMarker) {
+          this.addDragMarker();
+        }
+      }.bind(this)
+    );
+
+    this.map.on(
+      "contextmenu",
+      function (e) {
+        if (this.latLngs.length < 3) {
+          this.removeDragMarker();
+        }
+      }.bind(this)
+    );
   }
 }
 export { DrawPolygon };
